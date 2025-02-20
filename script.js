2c24f7dd4490f847b95e5a0c18c774b18b7f759a
matheus <matheus.com>
2025-02-20 15:20:16 -0300

    Projeto finalizado com sucesso! 🚀

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
-// Identificar qual tecla eu cliquei
+// EVENTOS
 
-//EVENTOS 
 document.body.addEventListener('keyup', (event) => {
-    tocarSom(event.code.toLocaleLowerCase() );
+    playSound(event.code.toLocaleLowerCase()) //Identifica a tecla pressionada, e transforma ela em minusculas
 });
+
+
+document.querySelectorAll('.key').forEach((key) => {
+    key.addEventListener('click', () => {
+        let keyCode = key.getAttribute('data-key'); // Pega o valor do atributo data-key
+        playSound(keyCode); // Toca o som da tecla clicada
+    });
+});
+
 
 document.querySelector('.composer button').addEventListener('click', () => {
-    let criandoComposicao = document.querySelector('#input').value;
-
-    if(criandoComposicao !== '') {
-        let songArray = criandoComposicao.split('');  //Transforma a string em lista array.
-        comecandoComposicao(songArray);
+    let song = document.querySelector('#input').value;
+    
+    if(song !== '') {
+        let songArray = song.split(''); //Transforma string em array
+        playComposition(songArray);
     }
-});
-
+})
 
 // FUNÇÕES
-function tocarSom(sound) {
-    let audioElement = document.querySelector(`#s_${sound}`);
-    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
-   
-    if(audioElement) {
-        audioElement.currentTime = 0; //Retira o delay de cada tecla
-        audioElement.play()
+
+function playSound(sound) {
+    let elementoDoAudio = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`); // Seleciona um elemento <div> no HTML que tenha um atributo data-key igual ao valor da variável sound
+
+    if(elementoDoAudio) {
+        elementoDoAudio.currentTime = 0; //Se tem um som sendo tocado reinicia o som para 0
+        elementoDoAudio.play(); //reproduz um audio quando é chamado
     }
 
     if(keyElement) {
-        keyElement.classList.add('active'); //Add a cor na tecla
+        keyElement.classList.add('active'); //adiciona classe CSS "active" ao elemento KeyElement
         
+
         setTimeout(() => {
-            keyElement.classList.remove('active'); //Remova a cor da tecla
-        },300);
+            keyElement.classList.remove('active'); //a cada 300 milesegundos o efeito da classe do css é retirado 
+        }, 300);
     }
 }
 
-// Função que recebe uma sequência de teclas e toca os sons um por um
-function comecandoComposicao(songArray) {
-    let wait = 0
-
+function playComposition(songArray) {
+    let wait = 0;
+    
     for(let songItem of songArray) {
         setTimeout(() => {
-            tocarSom(`key${songItem}`);
-        }, wait);
-
-        wait += 250;   
+            playSound(`key${songItem}`);
+        }, wait); 
+       
+        wait += 250;
+        //aumenta 250 milesegundos a cada tecla que é tocada
     }
 }
